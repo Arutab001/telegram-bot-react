010eed6526aa5d2b17021d3de1bd955e060108af
Artem <evangelionenjoyerzxc@gmail.com>
2024-09-11 23:23:45 +0300

    Fixed adaptive looks for smartphones. Still need to fix images in coinbox for pc monitors.

--- src/components/CoinBox.js
+++ src/components/CoinBox.js
@@ -4,12 +4,12 @@ import Coin1 from '../images/gecko_coin_rem 1.png'
 const CoinBox = (props) => {
     return (
         <div className="CoinBox">
-            <img src={Coin1} style={{height: "18vh", width: "11vw"}}/>
+            <img src={Coin1} style={{height: "18vh", width: "40%"}}/>
             <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
-                <div style={{fontSize: "xx-large", opacity: "70%"}}>
+                <div style={{fontSize: "x-large", opacity: "70%"}}>
                     $GMEME
                 </div>
-                <div style={{opacity: "50%", fontSize: "x-large"}}>
+                <div style={{opacity: "50%", fontSize: "large"}}>
                     500
                 </div>
             </div>
--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -5,7 +5,7 @@ const NewsBox = () => {
     return (
         <div className="NewsBox">
             <div >
-                <img src={News} style={{height: "60vh", width: "100%", borderRadius: "43px 43px 0 0"}}/>
+                <img src={News} style={{height: "30vh", width: "100%", borderRadius: "43px 43px 0 0"}}/>
             </div>
             <div style={{height: "8vh", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "x-large", opacity: "75%"}}>
                 Read
