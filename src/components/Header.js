8775bff612fc733efd6277974bac15e326f0a68b
Artem <evangelionenjoyerzxc@gmail.com>
2024-09-24 22:53:26 +0300

    Trying to get initdata

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {useUser} from "../UserContext.js";
 
 const Header = () => {
-
+    let tg = window.Telegram.WebApp;
     const {user, updateUser} = useUser();
 
     return (
         <div className="Header">
-            {Telegram.initDataUnsafe.user.id}
+            {tg.initDataUnsafe.user.id}
         </div>
     );
 };
