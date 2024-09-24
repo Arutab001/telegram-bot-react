8f0c4bcf264020c32d406dc800401f4085e25221
Artem <evangelionenjoyerzxc@gmail.com>
2024-09-24 23:21:34 +0300

    Check query

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     const {user, updateUser} = useUser();
     return (
         <div className="Header">
-            {tg.initDataUnsafe.query}
+            {tg.initData.query}
         </div>
     );
 };
