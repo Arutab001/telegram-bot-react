4588571e272c7a40c81103ac7a7386971ae19b3b
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-09 19:26:59 +0300

    Auth

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import axios from "axios";
-import TelegramAuth from "./components/TelegramAuth.js";
 
 
 const UserContext = createContext();
