06620b5656e7edb0d83e9e6421520dd3cc56cdc9
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-12 01:52:57 +0300

    Animation Casino

--- src/components/Casino/Casino.js
+++ src/components/Casino/Casino.js
@@ -88,12 +88,13 @@ const Casino = () => {
         const serverResponse = await getResultsFromServer();
         console.log(serverResponse.data.data);
         const emojiString = serverResponse.data.data.combination;
-        const combination = Array.from(emojiString);
+        const combination = emojiString.match(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]+/gu) || [];
 
-// Фильтруем массив, чтобы удалить нежелательные символы
-        const filteredCombination = combination.filter(emoji => emoji.trim() !== '');
 
-        console.log(filteredCombination);
+        const filteredCombination = combination.filter(emoji => slots.fruits.includes(emoji));
+
+        console.log(filteredCombination); // Выводим в консоль
+
         console.log(combination)
         setTimeout(() => {
             setRolling(false); // Останавливаем "вращение"
