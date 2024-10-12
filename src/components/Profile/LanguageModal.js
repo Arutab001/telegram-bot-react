e755f80b0009842587e0e2903eeecb1d73c5ad32
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-12 18:16:57 +0300

    Language

--- src/components/Profile/LanguageModal.js
+++ src/components/Profile/LanguageModal.js
@@ -31,12 +31,12 @@ const LanguageModal = ({show, onClose}) => {
 
     if (!show) return null;
 
-    const changeLanguage = async (lang) => {
+    const changeLanguage = async ({lang, e}) => {
         configureAxios();
         const response = await axios.post(`/language?lang=${lang}`, {
             headers: {'Authorization': `Bearer ${token}`}
         });
-        onClose();
+        onClose(e);
     }
 
     return (
@@ -44,7 +44,7 @@ const LanguageModal = ({show, onClose}) => {
             <div className="language-content">
                 <h2>{language.lang_menu}</h2>
                 {languages.map((lang) => (
-                    <MyBtn key={lang} text={capitalizeFirstLetter(lang)} onClick={changeLanguage}/>
+                    <MyBtn key={lang} text={capitalizeFirstLetter(lang)} onClick={changeLanguage(lang, e)}/>
                 ))}
             </div>
         </div>
