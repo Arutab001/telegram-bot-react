5ee6d6b09b898528e68dfeb8b69eeb304b7c5918
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-12 18:12:15 +0300

    Language

--- src/components/Profile/LanguageModal.js
+++ src/components/Profile/LanguageModal.js
@@ -9,10 +9,14 @@ const LanguageModal = ({show, onClose}) => {
     const {token} = useToken();
     const [languages, setLanguages] = useState([]);
     const {language} = useLanguage();
+
+    function configureAxios() {
+        axios.defaults.baseURL = 'https://geckoshi-stage.up.railway.app';
+    }
     // Имитируем запрос для получения доступных языков
     useEffect(() => {
         const fetchLanguages = async () => {
-            axios.defaults.baseURL = 'https://geckoshi-stage.up.railway.app';
+            configureAxios();
             const response = await axios.get(`/language/available`, {
                 headers: {'Authorization': `Bearer ${token}`}
             });
@@ -27,12 +31,20 @@ const LanguageModal = ({show, onClose}) => {
 
     if (!show) return null;
 
+    const changeLanguage = async (lang) => {
+        configureAxios();
+        const response = await axios.get(`/language?lang=${lang}`, {
+            headers: {'Authorization': `Bearer ${token}`}
+        });
+        onClose();
+    }
+
     return (
         <div className="language-overlay">
             <div className="language-content">
                 <h2>{language.lang_menu}</h2>
                 {languages.map((lang) => (
-                    <MyBtn key={lang} text={capitalizeFirstLetter(lang)} onClick={onClose}/>
+                    <MyBtn key={lang} text={capitalizeFirstLetter(lang)} onClick={changeLanguage}/>
                 ))}
             </div>
         </div>
