470673fb9dadb215b7c7e54d256b1d80101c5cdf
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-12 17:59:41 +0300

    Language

--- src/components/Casino/Base_Logic/LanguageContext.js
+++ src/components/Casino/Base_Logic/LanguageContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, {createContext, useContext, useEffect, useState} from 'react';
 import axios from "axios";
 import {useToken} from "./TelegramAuth.js";
 
@@ -9,36 +9,36 @@ export const useLanguage = () => {
     return useContext(LanguageContext);
 };
 
-const LanguageProvider = ({ children }) => {
+const LanguageProvider = ({children}) => {
     const [language, setLanguage] = useState(null);
     const {token} = useToken();
 
     useEffect(() => {
-        console.log("DAN DA DAN DAN DA DAN")
-        const fetchUserLanguage = async () => {
-            try {
-                axios.defaults.baseURL = 'https://geckoshi-stage.up.railway.app';
-                const response = await axios.get(`/language/pack`, {
-                    headers: {'Authorization': `Bearer ${token}`}
-                });
-                const data = await response.data;
-                console.log(data);
-                setLanguage(data);
+        if (!token) {
+            const fetchUserLanguage = async () => {
+                try {
+                    axios.defaults.baseURL = 'https://geckoshi-stage.up.railway.app';
+                    const response = await axios.get(`/language/pack`, {
+                        headers: {'Authorization': `Bearer ${token}`}
+                    });
+                    const data = await response.data;
+                    console.log(data);
+                    setLanguage(data);
 
-            } catch (error) {
-                console.error('Ошибка при выполнении запроса:', error);
-            }
-        };
-
-        fetchUserLanguage();
-    }, []);
+                } catch (error) {
+                    console.error('Ошибка при выполнении запроса:', error);
+                }
+            };
+            fetchUserLanguage();
+        }
+    }, [token]);
 
     const changeLanguage = (newLanguage) => {
         setLanguage(newLanguage);
     };
 
     return (
-        <LanguageContext.Provider value={{ language, changeLanguage }}>
+        <LanguageContext.Provider value={{language, changeLanguage}}>
             {children}
         </LanguageContext.Provider>
     );
