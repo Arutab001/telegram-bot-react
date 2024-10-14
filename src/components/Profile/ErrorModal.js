803ee4f7c32dbb3617fd38a3a70b7a2cd90f0100
Geckoshi <geckoshi.memecoin@gmail.com>
2024-10-15 00:05:43 +0300

    Update ErrorModal.js

--- src/components/Profile/ErrorModal.js
+++ src/components/Profile/ErrorModal.js
@@ -1,79 +1,53 @@
-import React, {useState} from 'react';
-import {useUser} from "../Base_Logic/UserContext.js";
-import classes from "./getPremium.module.css";
+import React from 'react';
+import "./ErrorModal.css"
 import {useLangProfile} from "../Base_Logic/UserLanguageProvider.js";
+import {useUser} from "../Base_Logic/UserContext.js";
 
 const translations = {
     english: {
-        PremiumPrice: "Premium Price",
-        BuyPremium: "Buy Premium",
+        PremiumWarning: "Premium Warning",
+        NotEnoughGMEME: `<p>You don't have <br/> enough $GMEME  <br/> to buy 'Premium'</p>`,
     },
     russian: {
-        PremiumPrice: "Цена Премиум",
-        BuyPremium: "Купить Премиум",
+        PremiumWarning: "Предупреждение о Премиум",
+        NotEnoughGMEME: `<p>У вас недостаточно <br/> $GMEME <br/> для покупки 'Премиум'</p>`,
     },
     german: {
-        PremiumPrice: "Premium Preis",
-        BuyPremium: "Premium Kaufen",
+        PremiumWarning: "Premium-Warnung",
+        NotEnoughGMEME: `<p>Sie haben nicht genügend <br/> $GMEME <br/> um 'Premium' zu kaufen</p>`,
     },
     turkish: {
-        PremiumPrice: "Premium Fiyatı",
-        BuyPremium: "Premium Satın Al",
+        PremiumWarning: "Premium Uyarısı",
+        NotEnoughGMEME: `<p>'Premium' satın almak için <br/> yeterli $GMEME'niz yok</p>`,
     },
 };
 
 
-const GetPremium = ({show, onClose, handleNot, setIsModalOpen, openError, closeModal}) => {
-    const [notificationVisible, setNotificationVisible] = useState(false);
+const ErrorModal = ({show, onClose}) => {
+
+    if (!show) return null;
+
+
     const { userLanguage } = useLangProfile();
     const {user, updateUser} = useUser();
 
     const localisation = translations[userLanguage] || translations[user.language] ||  translations.english
 
-
-    const getPremium = (e) => {
-        e.preventDefault();
-        if (!user.premium) {
-            if (user.referrals > 30000) {
-                handleNot(true);
-                setNotificationVisible(true);
-                user.premium = true;
-            } else {
-                closeModal(e);
-                openError(e);
-            }
-        }
-
-    };
-
-
-    if (!show) {
-        return null;
-    }
-
     return (
-        <div className={classes.modal_overlay}>
-            <div className={classes.modal_content}>
-                <div>
-                    <h2 style={{fontSize: "8vw"}}>{localisation.PremiumPrice}</h2>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         width="10vw" height="10vh" viewBox="0 0 86 86"
-                         fill="none" style={{position: "absolute", top: "-10", right: "-10"}}
-                         onClick={onClose}>
-                        <circle cx="43" cy="43" r="41.5" fill="#D9FFD2" stroke="#6D8069" stroke-width="3"/>
-                        <path d="M26.0415 26.0417L58.9582 58.9584M26.0415 58.9584L58.9582 26.0417" stroke="#6D8069"
-                              stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-                <div className={classes.Text}>
-                    <p>30 000 $GMEME</p>
-                    <div className={classes.buttonGetPremium}>
-                        <button onClick={getPremium} style={{backgroundColor: "#6D8069"}}>{localisation.BuyPremium}</button>
+        <div className="error-overlay">
+            <div className="error-content">
+                <h2>
+                    {localisation.PremiumWarning}
+                </h2>
+                <div style={{display: "flex", flexDirection: "column", height: "80%", alignItems: "center", paddingBottom: "5%"}}>
+                    <div style={{height: "50%", textAlign: "center"}}>
+                        {localisation.NotEnoughGMEME}
                     </div>
+                    <button onClick={onClose}>OK</button>
                 </div>
             </div>
         </div>
     );
 };
 
-export default GetPremium;
+export default ErrorModal;
