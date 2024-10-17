ea3b6c56a802d5ac942dc4b6bc3b64b7454a2f90
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-17 14:45:06 +0300

    Tasks Fix

--- src/components/Task/ModalComplete.js
+++ src/components/Task/ModalComplete.js
@@ -47,7 +47,8 @@ const ModalComplete = ({show, close, id, reward, openError}) => {
             axios.defaults.headers.common = { 'Authorization': `Bearer ${token}` };
             const response = await axios.post(`/task/done?task_id=${id}`);
             const data = response.data;
-            if (response.data.data.done_successfully === true){
+            console.log(data);
+            if (data.done_successfully === true){
                 close(e);
             }
             else {
