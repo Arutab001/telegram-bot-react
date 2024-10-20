8cf31bd21e6c9de5a2fc986c264d692b21d2abc4
Geckoshi <geckoshi.memecoin@gmail.com>
2024-10-20 20:47:36 +0300

    Update NewsBox.js

--- src/components/Main_Page/NewsBox.js
+++ src/components/Main_Page/NewsBox.js
@@ -2,38 +2,51 @@ import React, { useEffect, useState } from 'react';
 import "../../App.css";
 import NewsDefalut from '../../images/Frame 12.png';
 import axios from "axios";
+import {useToken} from "../Base_Logic/TelegramAuth.js";
+
 
 const NewsBox = () => {
     const [postUrl, setPostUrl] = useState("#");
     const [image, setImage] = useState(null);
-
+    const {token} = useToken();
 
     const fetchLatestPost = async () => {
-        try {
-           const response = await axios.get('channel/link');
-           console.log(response.data);
+        if (!token) return; // Защита от вызова без токена
 
+        try {
+            const response = await axios.get('https://geckoshi-prod.up.railway.app/channel/link', {
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+            console.log("Post URL:", response.data);
+            setPostUrl(response.data.url);
         } catch (error) {
             console.error("Error fetching latest post:", error);
         }
+
         try {
-            const response = await axios.get('channel/photo');
-            console.log(response.data);
+            const response = await axios.get('https://geckoshi-prod.up.railway.app/channel/photo?r=3', {
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+            console.log("Image URL:", response.data);
+            setImage(response.data.url);
         } catch (e) {
-            console.error(e);
+            console.error("Error fetching image:", e);
         }
     };
 
     useEffect(() => {
-        fetchLatestPost();
-    }, []);
+        if (token) {
+            fetchLatestPost();
+        }
+    }, [token]);
 
     return (
         <div className="NewsBox" onClick={() => window.open(postUrl, "_blank")} style={{ cursor: "pointer" }}>
             <div>
                 {image ? <img src={image} /> : <img src={NewsDefalut}/>}
             </div>
-            <div className="Text">Read</div>
+            <div className="Text">
+                Read</div>
         </div>
     );
 };
