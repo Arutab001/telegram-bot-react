e326be9eaf67cc3b3b0c4e3bdb5765d43418b913
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-25 02:10:39 +0300

    HalloWeen Update

--- src/components/Task/TaskContainer.js
+++ src/components/Task/TaskContainer.js
@@ -69,9 +69,9 @@ const TaskContainer = (props) => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 125 125"
                              fill="none">
                             <rect width="125" height="125" rx="62.5" fill="#6D8069"/>
-                            <path d="M53.3848 86.9233L77.6119 62.6962" stroke="#D9FFD2" stroke-width="10"
+                            <path d="M53.3848 86.9233L77.6119 62.6962" stroke="#725B40" stroke-width="10"
                                   stroke-linecap="round"/>
-                            <path d="M53.3848 37.9941L77.6155 62.2249" stroke="#D9FFD2" stroke-width="10"
+                            <path d="M53.3848 37.9941L77.6155 62.2249" stroke="#725B40" stroke-width="10"
                                   stroke-linecap="round"/>
                         </svg>
                     </Link>
