f1a49cc10d79c7bcd6323730a77b241635ca325c
Artem <evangelionenjoyerzxc@gmail.com>
2024-10-24 17:49:14 +0300

    Images For Tasks

--- src/components/Task/TaskPage.js
+++ src/components/Task/TaskPage.js
@@ -135,7 +135,7 @@ const TaskPage = () => {
     return (
         <div className="TaskPage">
             <div>
-                <img src={image || TaskImg}/>
+                <img src={TaskImg}/>
             </div>
             <div>
                 <h2 className="task-header">
