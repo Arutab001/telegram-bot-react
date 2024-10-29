d4783c656b7904b4068959093e0cca75cb7447b0
Artem <114524548+Arutab001@users.noreply.github.com>
2024-10-30 01:27:34 +0300

    Add files via upload

--- src/components/Base_Logic/Layout.js
+++ src/components/Base_Logic/Layout.js
@@ -1,18 +1,21 @@
-
 import React from 'react';
 import Header from '../Header.js';
 import Footer from '../Footer.js';
 import {Link, Outlet, useLocation, useNavigate} from 'react-router-dom';
+import Spider from "../Spider/Spider.js";
+import Pumpkin from "../Pumpkin/Pumpkin.js";
+
 
 const Layout = () => {
 
     return (
         <>
-            <Header />
+            <Header/>
+                <Pumpkin/>
             <main style={{marginBottom: "15vh"}}>
-                <Outlet />
+                <Outlet/>
             </main>
-            <Footer />
+            <Footer/>
         </>
     );
 };
