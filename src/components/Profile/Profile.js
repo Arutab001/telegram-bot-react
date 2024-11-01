bdd7de6e37ffa6f7146a510d68d6f95e48de2a53
Geckoshi <geckoshi.memecoin@gmail.com>
2024-11-01 21:02:08 +0300

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,83 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useUser } from '../hooks/useUser';
-import { useLanguage } from '../hooks/useLanguage';
-import { useLangProfile } from '../hooks/useLangProfile';
-import { useToken } from '../hooks/useToken';
-import MyBtn from './MyBtn';
-import PremiumNotification from './PremiumNotification';
-import LanguageModal from './LanguageModal';
-import ErrorModal from './ErrorModal';
-import defaultAvatar from '../assets/default-avatar.png';
-import translations from '../translations'; // убедитесь, что у вас есть файл с переводами
+import "./Profile.css";
+import MyBtn from "./MyBtn.js";
+import { useUser } from "../Base_Logic/UserContext.js";
+import GetPremium from "./GetPremium.js";
+import PremiumNotification from "./PremiumNotification.js";
+import LanguageModal from "./LanguageModal.js";
+import ErrorModal from "./ErrorModal.js";
+import defaultAvatar from "../../images/sticker 1.png";
+import { useLanguage } from "../Base_Logic/LanguageContext.js";
+import axios from "axios";
+import { useToken } from "../Base_Logic/TelegramAuth.js";
+import { useLangProfile } from "../Base_Logic/UserLanguageProvider.js";
+
+const translations = {
+    english: {
+        Info: "Account Info",
+        Name: "Name",
+        Id: "Your ID",
+        Premium: "Premium",
+        Ref: "Referrals",
+        Withdrawn: "Withdrawn",
+        WI: "Withdrawn Info",
+        Text1: "We will notify you in advance about payouts",
+        Text2: "MINIMUM WITHDRAWAL WILL BE 0 ON AIRDROP TODAY",
+        Change: "Change Language",
+        Balance: "$GMEME",
+        copySuccess: "ID copied!",
+        copyError: "Error copying ID",
+        ref_link: "Referral link"
+    },
+    russian: {
+        Info: "Информация об аккаунте",
+        Name: "Имя",
+        Id: "Ваш ID",
+        Premium: "Премиум",
+        Ref: "Рефералы",
+        Withdrawn: "Выведено",
+        WI: "Информация о выводе",
+        Text1: "Мы заранее уведомим вас о выплатах",
+        Text2: "МИНИМАЛЬНЫЙ ВЫВОД СЕГОДНЯ БУДЕТ 0 В AIRDROP",
+        Change: "Сменить язык",
+        Balance: "$GMEME",
+        copySuccess: "ID скопирован!",
+        copyError: "Ошибка при копировании ID",
+        ref_link: "Реферальная ссылка"
+    },
+    german: {
+        Info: "Konto Informationen",
+        Name: "Name",
+        Id: "Ihre ID",
+        Premium: "Premium",
+        Ref: "Empfehlungen",
+        Withdrawn: "Abgehoben",
+        WI: "Abhebungsinformationen",
+        Text1: "Wir werden Sie im Voraus über Auszahlungen informieren",
+        Text2: "DAS MINDESTABHEBEN WIRD HEUTE 0 BEI AIRDROP SEIN",
+        Change: "Sprache ändern",
+        Balance: "$GMEME",
+        copySuccess: "ID kopiert!",
+        copyError: "Fehler beim Kopieren der ID",
+        ref_link: "Empfehlungslink"
+    },
+    turkish: {
+        Info: "Hesap Bilgileri",
+        Name: "İsim",
+        Id: "ID'niz",
+        Premium: "Premium",
+        Ref: "Yönlendirmeler",
+        Withdrawn: "Çekildi",
+        WI: "Çekim Bilgisi",
+        Text1: "Ödemeler hakkında önceden sizi bilgilendireceğiz",
+        Text2: "BUGÜN AIRDROP'DA MİNİMUM ÇEKİM 0 OLACAK",
+        Change: "Dili Değiştir",
+        Balance: "$GMEME",
+        copySuccess: "ID kopyalandı!",
+        copyError: "ID kopyalanırken hata oluştu",
+        ref_link: "Yönlendirme bağlantısı"
+    },
+};
 
 const Profile = () => {
     const { user, updateUser, handleUserBalance } = useUser();
@@ -28,13 +96,6 @@ const Profile = () => {
 
     const [eventBalance, setEventBalance] = useState('');
 
-    // Функция для обрезки реферальной ссылки
-    const getShortRefLink = (refLink, maxLength = 20) => {
-        return refLink.length > maxLength
-            ? refLink.slice(0, 15) + "..." + refLink.slice(-5)
-            : refLink;
-    };
-
     const handleCopyId = async (param) => {
         if (!navigator.clipboard) {
             console.error("Clipboard API не поддерживается этим браузером");
@@ -46,8 +107,7 @@ const Profile = () => {
                 await navigator.clipboard.writeText(user.id.toString());
                 setCopySuccess(localisation.copySuccess);
             } else {
-                const fullRefLink = 'https://t.me/Geckoshi_bot/webapp?startapp=' + user.ref_link.toString();
-                await navigator.clipboard.writeText(fullRefLink);
+                await navigator.clipboard.writeText('https://t.me/Geckoshi_bot/webapp?startapp=' + user.ref_link.toString());
                 setRefCopySuccess(localisation.copySuccess);
             }
         } catch (e) {
@@ -77,7 +137,7 @@ const Profile = () => {
     };
 
     useEffect(() => {
-        const getBalance = async () => {
+        const getbalance = async () => {
             try {
                 const response = await axios.get('/coin/balance');
                 if (response.status === 200) {
@@ -88,7 +148,6 @@ const Profile = () => {
                 console.error(e);
             }
         };
-
         const getEventBalance = async () => {
             try {
                 const response = await axios.get('/event-bonus/balance?event_id=1');
@@ -99,104 +158,7 @@ const Profile = () => {
                 console.error(e);
             }
         }
-
-        getBalance();
-        getEventBalance();
-    }, []);
-
-    const formatNumber = (num) => {
-        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-    };
-const Profile = () => {
-    const { user, updateUser, handleUserBalance } = useUser();
-    const { language } = useLanguage();
-    const { userLanguage } = useLangProfile();
-    const { token } = useToken();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
-    const [isLangModalOpen, setIsLangModalOpen] = useState(false);
-    const [isErrorVisible, setErrorVisible] = useState(false);
-    const [avatar, setAvatar] = useState(defaultAvatar);
-    const [copySuccess, setCopySuccess] = useState('');
-    const [refCopySuccess, setRefCopySuccess] = useState('');
-
-    const localisation = translations[userLanguage] || translations[user.language] || translations.english;
-
-    const [eventBalance, setEventBalance] = useState('');
-
-    // Функция для обрезки реферальной ссылки
-    const getShortRefLink = (refLink, maxLength = 20) => {
-        return refLink.length > maxLength
-            ? refLink.slice(0, 15) + "..." + refLink.slice(-5)
-            : refLink;
-    };
-
-    const handleCopyId = async (param) => {
-        if (!navigator.clipboard) {
-            console.error("Clipboard API не поддерживается этим браузером");
-            param === 'id' ? setCopySuccess(localisation.copyError) : setRefCopySuccess(localisation.copyError);
-            return;
-        }
-        try {
-            if (param === 'id') {
-                await navigator.clipboard.writeText(user.id.toString());
-                setCopySuccess(localisation.copySuccess);
-            } else {
-                const fullRefLink = 'https://t.me/Geckoshi_bot/webapp?startapp=' + user.ref_link.toString();
-                await navigator.clipboard.writeText(fullRefLink);
-                setRefCopySuccess(localisation.copySuccess);
-            }
-        } catch (e) {
-            console.error(e);
-            param === 'id' ? setCopySuccess(localisation.copyError) : setRefCopySuccess(localisation.copyError);
-        }
-    };
-
-    useEffect(() => {
-        if (copySuccess || refCopySuccess) {
-            const timer = setTimeout(() => {
-                setCopySuccess('');
-                setRefCopySuccess('');
-            }, 3000);
-            return () => clearTimeout(timer);
-        }
-    }, [copySuccess, refCopySuccess]);
-
-    const openLang = (e) => {
-        e.preventDefault();
-        setIsLangModalOpen(true);
-    };
-
-    const closeLang = (e) => {
-        e.preventDefault();
-        setIsLangModalOpen(false);
-    };
-
-    useEffect(() => {
-        const getBalance = async () => {
-            try {
-                const response = await axios.get('/coin/balance');
-                if (response.status === 200) {
-                    handleUserBalance(response.data.data.GMEME);
-                }
-            } catch (e) {
-                console.log("Balance error:");
-                console.error(e);
-            }
-        };
-
-        const getEventBalance = async () => {
-            try {
-                const response = await axios.get('/event-bonus/balance?event_id=1');
-                if (response.status === 200) {
-                    setEventBalance(response.data.data.balance);
-                }
-            } catch (e) {
-                console.error(e);
-            }
-        }
-
-        getBalance();
+        getbalance();
         getEventBalance();
     }, []);
 
@@ -228,39 +190,32 @@ const Profile = () => {
                     </div>
                     <div className="Block">
                         <span>{localisation.Premium}: </span> {user.premium ? '✓' : '✗'} <br />
-                        <span>{localisation.ref_link}: </span>
+                        <span> {localisation.ref_link}: </span>
                         <span onClick={() => handleCopyId('ref_link')}
                             style={{ cursor: 'pointer', textDecoration: 'underline' }}
-                        >
-                            {getShortRefLink(user.ref_link)}
-                        </span>
+                        >{user.ref_link}</span>
                         {refCopySuccess && <span>{refCopySuccess}</span>} <br />
-                        <span>{localisation.Ref}: </span> {user.referrals} <br />
-                        <span>{localisation.Withdrawn}: </span> {user.withdraw} <br />
+                                                <span>{localisation.Ref}: </span> {user.referrals} <br />
+                        <span>{localisation.Withdrawn}: </span> {user.withdrawn} <br />
+                        <span>{localisation.WI}: </span> {user.withdrawn_info} <br />
+                        <span>{localisation.Text1}</span> <br />
+                        <span>{localisation.Text2}</span> <br />
                     </div>
                     <div className="Block">
                         <span>{localisation.Balance}: </span> {formatNumber(user.balance)} <br />
-                        <span>$BMEME:</span> 0 <br />
-                        <span>🎃🎃🎃: </span> {eventBalance} <br />
+                        <span>Event Balance: </span> {formatNumber(eventBalance)} <br />
                     </div>
-                    <div className="Block">
-                        <MyBtn text={localisation.WI} onClick={() => setIsVisible(true)} />
-                        <span>{localisation.Text1}</span><br />
-                        <span>{localisation.Text2}</span><br />
-                    </div>
-                    <div className="Block">
-                        <MyBtn text={localisation.Change} onClick={openLang} />
+                    <div className="buttons">
+                        <MyBtn onClick={() => setIsModalOpen(true)} text={localisation.Change} />
+                        <GetPremium />
                     </div>
                 </div>
             </div>
-            {isLangModalOpen && (
-                <LanguageModal isOpen={isLangModalOpen} closeModal={closeLang} />
-            )}
-            {isErrorVisible && (
-                <ErrorModal onClose={() => setErrorVisible(false)} />
-            )}
+            {isLangModalOpen && <LanguageModal onClose={closeLang} />}
+            {isErrorVisible && <ErrorModal onClose={() => setErrorVisible(false)} />}
         </div>
     );
 };
 
 export default Profile;
+
