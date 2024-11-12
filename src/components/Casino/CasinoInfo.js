222ea55ddbcebf24890f316e7ca07c6e217aed2c
Artem <114524548+Arutab001@users.noreply.github.com>
2024-11-12 10:26:24 +0300

    Add files via upload

--- src/components/Casino/CasinoInfo.js
+++ src/components/Casino/CasinoInfo.js
@@ -31,7 +31,7 @@ const CasinoInfo = () => {
     return (
         <div className={`CasinoInfo ${isVisible ? 'show' : 'hide'}`}>
             {localisation}
-            <svg onClick={setClose} xmlns="http://www.w3.org/2000/svg" width="36" height="21" viewBox="0 0 36 21" fill="none">
+            <svg onClick={setClose} xmlns="http://www.w3.org/2000/svg" width="15" height="10" viewBox="0 0 36 21" fill="none">
                 <path d="M33 18L18.0023 3.00227" stroke="#364035" stroke-width="5" stroke-linecap="round"/>
                 <path d="M2.71045 18L17.7104 3" stroke="#364035" stroke-width="5" stroke-linecap="round"/>
             </svg>
