75927737ccc830f160064f1a0ad1791b9308f3ce
Artem <evangelionenjoyerzxc@gmail.com>
2024-12-15 20:25:55 +0300

    Telegram Params

--- src/components/Base_Logic/StartRedirect.js
+++ src/components/Base_Logic/StartRedirect.js
@@ -4,17 +4,19 @@ import { useNavigate } from "react-router-dom";
 const StartRedirect = () => {
     const navigate = useNavigate();
 
+    // Функция для извлечения параметров из декодированного хэша
     const getHashParam = (param) => {
-        const hash = window.location.hash.slice(1); // Убираем '#'
-        console.log('Full hash:', hash); // Выведи весь хэш
-        const params = new URLSearchParams(hash);
-        return params.get(param);
+        const hash = window.location.hash.slice(1); // Убираем '#' из хэша
+        const decodedHash = decodeURIComponent(hash); // Декодируем хэш
+        console.log('Decoded Hash:', decodedHash); // Выводим декодированный хэш для диагностики
+        const params = new URLSearchParams(decodedHash); // Создаем объект URLSearchParams из декодированного хэша
+        return params.get(param); // Извлекаем нужный параметр
     };
 
-
     useEffect(() => {
-        const startApp = getHashParam('startapp'); // Извлекаем параметр 'startapp' из хэша
-        console.log("Param:" + startApp)
+        const startApp = getHashParam('startapp'); // Извлекаем параметр startapp из хэша
+        console.log('StartApp:', startApp); // Выводим startapp в консоль для диагностики
+
         if (startApp) {
             navigate(`/ActivateCheck/${startApp}`);
         } else {
