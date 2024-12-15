62de151d16a1ecfeeecbcd4b71ed32a2af4aad5b
Artem <evangelionenjoyerzxc@gmail.com>
2024-12-15 20:13:04 +0300

    Telegram Params

--- src/App.js
+++ src/App.js
@@ -1,51 +1,78 @@
+// App.js
 import './App.css';
-import { useEffect, useState } from 'react';
-import {createBrowserRouter, createHashRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
 import Layout from './components/Base_Logic/Layout.js';
 import Home from './components/Main_Page/Home.js';
 import Casino from './components/Casino/Casino.js';
 import Profile from './components/Profile/Profile.js';
 import Task from './components/Task/Task.js';
 import TaskPage from "./components/Task/TaskPage.js";
+import {TelegramAuth} from "./components/Base_Logic/TelegramAuth.js";
 import CheckMainPage from "./components/Checks/Mainpage/CheckMainPage.js";
 import CreateCheck from "./components/Checks/CreateCheck/CreateCheck.js";
 import Check from "./components/Checks/Check/Check.js";
-import ActivateCheck from "./components/Checks/ActivateCheck/ActivateCheck.js";
+import {useUser} from "./components/Base_Logic/UserContext.js";  // Импортируем useUser
 import LoadingScreen from "./components/LoadingScreen/LoadingScreen.js";
 import StartRedirect from "./components/Base_Logic/StartRedirect.js";
 
 const tg = window.Telegram.WebApp;
 
-function App() {
-    const [isLoading, setIsLoading] = useState(true);
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout/>}>
+            <Route index element={<StartRedirect />} />
+            <Route path="Home" element={<Home/>}/>
+            <Route path="Casino" element={<Casino/>}/>
+            <Route path="Profile" element={<Profile/>}/>
+            <Route path="Tasks" element={<Task/>}/>
+            <Route path="TaskPage" element={<TaskPage/>}/>
+            <Route path="CheckMainPage" element={<CheckMainPage/>}/>
+            <Route path="CreateCheck" element={<CreateCheck/>}/>
+            <Route path="Check" element={<Check/>}/>
+        </Route>
+    )
+);
 
+function App() {
     useEffect(() => {
-        tg.ready(); // Telegram WebApp готов к работе
-        setIsLoading(false);
+        tg.ready();
     }, []);
 
-    if (isLoading) {
-        return <LoadingScreen />;
-    }
+    document.addEventListener('wheel', function (event) {
+        if (event.ctrlKey) {
+            event.preventDefault();
+        }
+    }, {passive: false});
 
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            <Route path="/" element={<Layout />}>
-                <Route index element={<StartRedirect />} />
-                <Route path="Home" element={<Home />} />
-                <Route path="Casino" element={<Casino />} />
-                <Route path="Profile" element={<Profile />} />
-                <Route path="Tasks" element={<Task />} />
-                <Route path="TaskPage" element={<TaskPage />} />
-                <Route path="CheckMainPage" element={<CheckMainPage />} />
-                <Route path="CreateCheck" element={<CreateCheck />} />
-                <Route path="Check" element={<Check />} />
-                <Route path="ActivateCheck/:checkId" element={<ActivateCheck />} />
-            </Route>
-        )
-    );
+    document.addEventListener('keydown', function (event) {
+        if (event.ctrlKey && (event.key === '+' || event.key === '-' || event.key === '0')) {
+            event.preventDefault();
+        }
+    });
 
-    return <RouterProvider router={router} />;
+    const [isLoading, setIsLoading] = useState(true);
+    const {user} = useUser();
+
+    useEffect(() => {
+        if (user.id !== 0) {
+            setIsLoading(false);
+        }
+    }, [user]);
+
+    const onClose = () => {
+        tg.close();
+    };
+
+    return (
+        <>
+            {isLoading ? (
+                <LoadingScreen onComplete={() => {
+                    setIsLoading(false)
+                }}/>
+            ) : (<RouterProvider router={router}/>)}
+        </>
+    )
 }
 
 export default App;
--- src/components/Base_Logic/StartRedirect.js
+++ src/components/Base_Logic/StartRedirect.js
@@ -3,10 +3,14 @@ import { useNavigate } from "react-router-dom";
 
 const StartRedirect = () => {
     const navigate = useNavigate();
+    const getQueryParam = (param) => {
+        const urlParams = new URLSearchParams(location.search);
+        return urlParams.get(param); // Извлекаем значение параметра startapp
+    };
 
+    const startAppParam = getQueryParam('startapp');
     useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        const startApp = params.get('startapp');
+        const startApp = getQueryParam('startapp');
 
         if (startApp) {
             navigate(`/ActivateCheck/${startApp}`);
